a03851b8216cd7d8ead983ab0fd3727a0ab0c968
Francisco Casas <franciscojacb@gmail.com>
2017-12-03 19:56:43 -0300

    Removed sphinx extensions from docs/config.py that triggered an error on readthedocs.org.

--- doc/conf.py
+++ doc/conf.py
@@ -40,7 +40,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+#    'sphinx.ext.githubpages',
     'sphinx_automodapi.automodapi',
     'numpydoc'
 ]
